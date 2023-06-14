e15ac13bd2021b2fe19ce43f3c87ec776e680cfa
Gustavo Alencar <gustavorique@live.com>
2023-06-14 20:08:54 -0300

    Classe DevConfig criada

---
+++ src/main/java/com/udemy/demo/config/DevConfig.java
@@ -0,0 +1,28 @@
+package com.udemy.demo.config;
+
+import com.udemy.demo.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String ddl;
+
+    @Bean
+    public boolean instanciaDB() {
+        if(ddl.equals("create")){
+            this.dbService.instanciaDB();
+        }
+        return false;
+    }
+
+}
